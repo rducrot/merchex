bff3eb75f532cf51fe61bf4b8a5fb9cc3c12237e
rducrot <ducrot.romain@gmail.com>
2022-11-15 13:54:34 +0100

    Page de suppression des objets

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -36,14 +36,23 @@ def band_update(request, band_id):
     band = Band.objects.get(id=band_id)
 
     if request.method == "POST":
-        form = BandForm(request.POST)
+        form = BandForm(request.POST, instance=band)
         if form.is_valid:
-            band = form.save()
-            return redirect('band-detail', band.id)
+            form.save()
+            return redirect('band-detail', band_id)
 
     form = BandForm(instance=band)
     return render(request, "listings/band_update.html",
                   {"form": form})
+
+
+def band_delete(request, band_id):
+    band = Band.objects.get(id=band_id)
+    if request.method == "POST":
+        band.delete()
+        return redirect('band-list')
+    return render(request, "listings/band_delete.html",
+                  {"band": band})
 
 
 def listing_list(request):
@@ -76,14 +85,23 @@ def listing_update(request, listing_id):
     listing = Listing.objects.get(id=listing_id)
 
     if request.method == "POST":
-        form = ListingForm(request.POST)
-        listing = form.save()
-        return redirect("listing-detail", listing.id)
+        form = ListingForm(request.POST, instance=listing)
+        form.save()
+        return redirect("listing-detail", listing_id)
 
     form = ListingForm(instance=listing)
     return render(request,
                   "listings/listing_update.html",
                   {"form": form})
+
+
+def listing_delete(request, listing_id):
+    listing = Listing.objects.get(id=listing_id)
+    if request.method == "POST":
+        listing.delete()
+        return redirect('listing-list')
+    return render(request, "listings/listing_delete.html",
+                  {"listing": listing})
 
 
 def contact(request):
